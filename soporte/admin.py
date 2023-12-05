1b8208b4895f2c0ac0f8dd26ff6c58be338bfed6
Junior Mota <motaibhaltair@gmail.com>
2023-12-05 15:22:00 -0500

    Cambios al models y al admin

--- soporte/admin.py
+++ soporte/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import ExtendedUser, Empresa, Rol, TicketActualizacion, TicketDesarrollo, TicketSoporte, ActividadPrincipal, ActividadSecundaria
+
+# Personalizar la visualización de ExtendedUser en el admin
+class ExtendedUserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            'Información Adicional',
+            {
+                'fields': (
+                    'direccion',
+                    'telefono1',
+                    'telefono2',
+                    'email',
+                    'idEmpresa',
+                    'idRol',
+                ),
+            },
+        ),
+    )
+
+# Registrar los modelos en el admin
+admin.site.register(ExtendedUser, ExtendedUserAdmin)
+admin.site.register(Empresa)
+admin.site.register(Rol)
+admin.site.register(TicketActualizacion)
+admin.site.register(TicketDesarrollo)
+admin.site.register(TicketSoporte)
+admin.site.register(ActividadPrincipal)
+admin.site.register(ActividadSecundaria)
--- soporte/models.py
+++ soporte/models.py
@@ -1,21 +1,85 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class Usuario(models.Model):
-    id_plataforma = models.AutoField(primary_key=True)
-    nombre_plataforma = models.CharField(max_length=255)
-    usuario_anydesk = models.CharField(max_length=255)
-    password_anydesk = models.CharField(max_length=255)
-    instancia_sql = models.CharField(max_length=255)
-    usuario_sql = models.CharField(max_length=255)
-    contraseña_sql = models.CharField(max_length=255)
-    host_remoto = models.CharField(max_length=255)
-    usuario_remoto = models.CharField(max_length=255)
-    password_remoto = models.CharField(max_length=255)
-    nombre_red_radmin = models.CharField(max_length=255)
-    password_radmin = models.CharField(max_length=255)
-    url_radmin = models.CharField(max_length=255)
-    usuario_equipo = models.CharField(max_length=255)
-    contrasena_equipo = models.CharField(max_length=255)
+class Empresa(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombreEmpresa = models.CharField(max_length=255)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    direccion = models.CharField(max_length=255)
+    telefono = models.IntegerField()
+    email = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.nombre_plataforma
+class Rol(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=255)
+
+class ExtendedUser(AbstractUser):
+    direccion = models.CharField(max_length=255, null=True, blank=True, default='')
+    telefono1 = models.IntegerField(null=True, blank=True)
+    telefono2 = models.IntegerField(null=True, blank=True)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    idEmpresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, null=True, blank=True)
+    idRol = models.ForeignKey(Rol, on_delete=models.CASCADE, null=True, blank=True)
+
+class TicketActualizacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    idAgente = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE)
+    idEmpresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    fechaInicio = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacion = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacionReal = models.DateTimeField(auto_now_add=True)
+    horasDiariasAsignadas = models.DecimalField(max_digits=5, decimal_places=2)
+    moduloActualizar = models.CharField(max_length=255)
+    descripcionGeneral = models.CharField(max_length=255)
+    observaciones = models.CharField(max_length=255)
+    prioridad = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+
+# Modelo TicketDesarrollo
+class TicketDesarrollo(models.Model):
+    id = models.AutoField(primary_key=True)
+    idAgente = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE, related_name='ticket_desarrollo_agente')
+    idSolicitante = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE, related_name='ticket_desarrollo_solicitante')
+    nombreProyecto = models.CharField(max_length=255)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    fechaInicio = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacion = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacionReal = models.DateTimeField(auto_now_add=True)
+    descripcionActividadPrincipal = models.ForeignKey('ActividadPrincipal', on_delete=models.CASCADE)
+    idActividadSecundaria = models.ForeignKey('ActividadSecundaria', on_delete=models.CASCADE)
+
+# Modelo TicketSoporte
+class TicketSoporte(models.Model):
+    id = models.AutoField(primary_key=True)
+    idAgente = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE, related_name='ticket_soporte_agente')
+    idSolicitante = models.ForeignKey(ExtendedUser, on_delete=models.CASCADE, related_name='ticket_soporte_solicitante')
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    fechaInicio = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacion = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacionReal = models.DateTimeField(auto_now_add=True)
+    comentario = models.CharField(max_length=255)
+    prioridad = models.CharField(max_length=100)
+    observacion = models.CharField(max_length=255)
+    estado = models.CharField(max_length=100)
+    facturar = models.BooleanField()
+
+class ActividadPrincipal(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=255)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    fechaInicio = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacion = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacionReal = models.DateTimeField(auto_now_add=True)
+    idTicketDesarrollo = models.ForeignKey(TicketDesarrollo, on_delete=models.CASCADE)
+    idTicketSecundario = models.ForeignKey('ActividadSecundaria', on_delete=models.CASCADE)
+
+
+class ActividadSecundaria(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=255)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    fechaInicio = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacion = models.DateTimeField(auto_now_add=True)
+    fechaFinalizacionReal = models.DateTimeField(auto_now_add=True)
+    idActividadPrincipal = models.ForeignKey(ActividadPrincipal, on_delete=models.CASCADE)
