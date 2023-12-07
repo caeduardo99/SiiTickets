b191428716edfa36fd0076ce755f091707d8de69
Carlos Soria <carlossoria1999@gmail.com>
2023-12-07 10:21:12 -0500

    update models

--- soporte/models.py
+++ soporte/models.py
@@ -2,12 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 from django.db import models
 
+class EstadosTicket(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=30)
+
+
 class Empresa(models.Model):
     id = models.AutoField(primary_key=True)
     nombreEmpresa = models.CharField(max_length=255)
     fechaCreacion = models.DateTimeField(auto_now_add=True)
     direccion = models.CharField(max_length=255)
-    telefono = models.IntegerField()
+    telefono = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
 
 class Rol(models.Model):
@@ -18,16 +23,17 @@ class TicketActualizacion(models.Model):
     id = models.AutoField(primary_key=True)
     idAgente = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     idEmpresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
-    fechaCreacion = models.DateTimeField(auto_now_add=True)
-    fechaInicio = models.DateTimeField(auto_now_add=True)
-    fechaFinalizacion = models.DateTimeField(auto_now_add=True)
-    fechaFinalizacionReal = models.DateTimeField(auto_now_add=True)
+    fechaCreacion = models.DateTimeField()
+    fechaInicio = models.DateTimeField()
+    fechaFinalizacion = models.DateTimeField()
+    fechaFinalizacionReal = models.DateTimeField()
     horasDiariasAsignadas = models.DecimalField(max_digits=5, decimal_places=2)
     moduloActualizar = models.CharField(max_length=255)
     descripcionGeneral = models.CharField(max_length=255)
     observaciones = models.CharField(max_length=255)
     prioridad = models.CharField(max_length=100)
-    estado = models.CharField(max_length=100)
+    estado = models.ForeignKey(EstadosTicket, on_delete=models.CASCADE)  # Cambio aquí
+    facturar = models.BooleanField()
 
 # Modelo TicketDesarrollo
 class TicketDesarrollo(models.Model):
@@ -35,17 +41,19 @@ class TicketDesarrollo(models.Model):
     idAgente = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='ticket_desarrollo_agente')
     idSolicitante = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='ticket_desarrollo_solicitante')
     nombreProyecto = models.CharField(max_length=255)
-    fechaCreacion = models.DateTimeField(auto_now_add=True)
-    fechaInicio = models.DateTimeField(auto_now_add=True)
-    fechaFinalizacion = models.DateTimeField(auto_now_add=True)
-    fechaFinalizacionReal = models.DateTimeField(auto_now_add=True)
+    fechaCreacion = models.DateTimeField()
+    fechaInicio = models.DateTimeField()
+    fechaFinalizacion = models.DateTimeField()
+    fechaFinalizacionReal = models.DateTimeField()
     descripcionActividadPrincipal = models.ForeignKey('ActividadPrincipal', on_delete=models.CASCADE)
     idActividadSecundaria = models.ForeignKey('ActividadSecundaria', on_delete=models.CASCADE)
+    estado = models.ForeignKey(EstadosTicket, on_delete=models.CASCADE)  # Cambio aquí
+    facturar = models.BooleanField()
 
 class Solicitante(models.Model):
     id = models.AutoField(primary_key=True)
     nombreApellido = models.CharField(max_length=255)
-    telefonoSolicitante = models.DecimalField(max_digits=10, decimal_places=0)
+    telefonoSolicitante = models.CharField(max_length=255)
     idEmpresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
 
 # Modelo TicketSoporte
@@ -60,7 +68,7 @@ class TicketSoporte(models.Model):
     comentario = models.CharField(max_length=255)
     prioridad = models.CharField(max_length=100)
     observacion = models.CharField(max_length=255)
-    estado = models.CharField(max_length=100)
+    estado = models.ForeignKey(EstadosTicket, on_delete=models.CASCADE)  # Cambio aquí
     facturar = models.BooleanField()
 
 class ActividadPrincipal(models.Model):
